53be148c49015fb50efc13f49dd57afa12513430
Jamarius Mario Thorpe <mariothorpe22@gmail.com>
2025-07-16 18:17:20 -0400

    fix: commented out unecessary console.log statement in 03-modules.js

--- 01-node-tutorial/answers/03-modules.js
+++ 01-node-tutorial/answers/03-modules.js
@@ -1,7 +1,7 @@
 const names = require('./04-names');
 const sayHello = require('./05-utils');
 const data = require('./06-alternative-flavor');
-console.log(data);
+// console.log(data);
 require('./07-mind-grenade');
 sayHello(names.mario);
 sayHello(names.tommy);
